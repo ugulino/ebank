787668c9d6431576e28342ac589f896f72bc78b4
Fabiano <ugulino@hotmail.com>
2020-11-20 13:47:02 -0300

    Initilize project

---
+++ src/test/java/io/app/ebank/EbankApplicationTests.java
@@ -0,0 +1,13 @@
+package io.app.ebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
